7859a030b49c64fefe7858c5c29ada9cbab69d40
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-09 15:53:27 +0530

    code section almost done

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,13 @@
 <template>
-	<div>
-		<h1>{{ emoji }} {{ name }}</h1>
+	<div class="card">
+		<a :href="link" target="_blank" rel="noopener noreferrer">
+			<div>
+				<h1>{{ emoji }} {{ title }}</h1>
+				<div class="col">
+					<i class="fas fa-external-link-alt fa-lg"></i>
+				</div>
+			</div>
+		</a>
 	</div>
 </template>
 
@@ -9,9 +16,44 @@ export default {
 	name: "Card",
 	props: {
 		emoji: String,
-		name: String,
+		title: String,
+		link: String,
 	},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+	background-color: #1b405e;
+	border-radius: 8px;
+	padding: 0.5rem 1rem;
+}
+
+.card:hover {
+	background-color: #15334b;
+	transition: background-color 0.2s ease;
+	cursor: pointer;
+}
+.col {
+	display: flex;
+	/* height: 100%; */
+	justify-content: center;
+	align-items: center;
+}
+
+.card div {
+	display: flex;
+	justify-content: space-between;
+}
+
+h1,
+a {
+	text-decoration: none;
+	color: white;
+}
+
+h1 {
+	font-weight: 500;
+	font-size: 1.8rem;
+}
+</style>
--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,16 +1,200 @@
 <template>
 	<div>
-		<Card :emoji="hello" :name="mcow.ml"></Card>
-		<h1>hello</h1>
+		<div class="row">
+			<h1>Code</h1>
+			<div>
+				<button v-on:click="expandDiv">
+					<i class="fas fa-chevron-down fa-lg"></i>
+				</button>
+			</div>
+		</div>
+
+		<div class="cards">
+			<Card
+				v-for="card in cards"
+				:key="card.title"
+				:title="card.title"
+				:emoji="card.emoji"
+				:link="card.link"
+			></Card>
+		</div>
 	</div>
 </template>
 
 <script>
-import Card from "@/components/Card.vue";
+import Card from "./Card.vue";
 export default {
 	name: "Code",
 	components: { Card },
+	data() {
+		return {
+			cards: [
+				{
+					title: "mcow.ml",
+					emoji: " 🐮",
+					link: "https://www.mcow.ml",
+				},
+				{
+					title: "Gravity",
+					emoji: " 🌎",
+					link: "https://vanajmoorthy.com/gravity",
+				},
+				{
+					title: "Blog",
+					emoji: " ✒️",
+					link: "https://vanajmoorthy.com/blog",
+				},
+				{
+					title: "Dogs",
+					emoji: " 🐶",
+					link: "https://vanajmoorthy.com/dogs",
+				},
+				{
+					title: "Morph",
+					emoji: " 💠",
+					link: "https://vanajmoorthy.com/morph",
+				},
+				{
+					title: "cshell",
+					emoji: " 🐚",
+					link: "https://vanajmoorthy.com/gh/cshell",
+				},
+				{
+					title: "Speechy",
+					emoji: " 🐢",
+					link: "https://vanajmoorthy.com/gh/speechylang",
+				},
+				{
+					title: "Short",
+					emoji: " ✂️",
+					link: "https://vanajmoorthy.com/gh/short",
+				},
+				{
+					title: "Obstacles",
+					emoji: " ▶️",
+					link: "https://vanajmoorthy.com/obstacles",
+				},
+				{
+					title: "Tic Tac Toe",
+					emoji: " ❌",
+					link: "https://vanajmoorthy.com/tictactoe",
+				},
+				{
+					title: "Pi2",
+					emoji: " 🥧",
+					link: "https://vanajmoorthy.com/pi2",
+				},
+				{
+					title: "Yoda",
+					emoji: " 👽",
+					link: "https://vanajmoorthy.com/yoda",
+				},
+				{
+					title: "Pi",
+					emoji: " 🥧",
+					link: "https://vanajmoorthy.com/pi",
+				},
+				{
+					title: "Keylogger",
+					emoji: " 🕵️",
+					link: "https://vanajmoorthy.com/gi/keylogger",
+				},
+				{
+					title: "Enigma",
+					emoji: " ❓",
+					link: "https://vanajmoorthy.com/enigma",
+				},
+				{
+					title: "Spam Bot",
+					emoji: " 🤖",
+					link: "https://vanajmoorthy.com/gh/spambot",
+				},
+				{
+					title: "Visualise",
+					emoji: " 🎤",
+					link: "https://vanajmoorthy.com/link",
+				},
+				{
+					title: "Graph",
+					emoji: " 📈",
+					link: "https://vanajmoorthy.com/graph",
+				},
+				{
+					title: "Paint",
+					emoji: " 🎨",
+					link: "https://vanajmoorthy.com/paint",
+				},
+				{
+					title: "JockyCipher",
+					emoji: " 🔑",
+					link: "https://vanajmoorthy.com/jockycipher",
+				},
+				{
+					title: "Clock",
+					emoji: " 🕒",
+					link: "https://vanajmoorthy.com/clock",
+				},
+				{
+					title: "Snake",
+					emoji: " 🍎",
+					link: "https://vanajmoorthy.com/snake",
+				},
+			],
+		};
+	},
+	methods: {
+		expandDiv() {
+			const div = document.querySelector(".cards");
+			console.log(div);
+			div.style.height = "100%";
+		},
+	},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.cards {
+	grid-gap: 1rem;
+	margin: 0 auto;
+	display: grid;
+	height: 320px;
+	overflow: hidden;
+}
+
+@media (min-width: 600px) {
+	.cards {
+		grid-template-columns: repeat(2, 1fr);
+	}
+}
+
+h1 {
+	font-size: 1.8rem;
+	font-weight: 500;
+	color: white;
+	border-bottom: 10px solid #1b405e;
+	line-height: 0.4;
+	width: 70px;
+}
+.row {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+
+svg {
+	color: white;
+}
+
+.row button {
+	border: none;
+	padding: 10px;
+	border-radius: 8px;
+	background-color: #1b405e;
+}
+
+.row button:hover {
+	background-color: #15334b;
+	transition: background-color ease 0.2s;
+	cursor: pointer;
+}
+</style>
