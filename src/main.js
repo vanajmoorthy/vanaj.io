852dfcdf96dd7c5f3050a21565b4aa8a8f7ac145
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-09 10:52:42 +0530

    favicon replace

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
