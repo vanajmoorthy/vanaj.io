d6edf2fa8b60f10dbe3c609c9d6c1e18b46cc89a
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-10 20:34:15 +0530

    stuff

--- src/components/ColophonMusic.vue
+++ src/components/ColophonMusic.vue
@@ -34,6 +34,7 @@ export default {
 		fetch(url)
 			.then((res) => res.json())
 			.then((data) => {
+				console.log(data);
 				function removeDuplicates(arr) {
 					const result = [];
 					const duplicatesIndices = [];
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
-	render: function(h) {
+    router,
+
+    render: function(h) {
 		return h(App);
-	},
+	}
 }).$mount("#app");
