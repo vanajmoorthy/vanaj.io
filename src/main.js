b55db9e796b1387beb60fa8a35c53b4d283f1e38
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-10 20:52:59 +0530

    revert 404 attempt

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-
-    render: function(h) {
+	render: function(h) {
 		return h(App);
-	}
+	},
 }).$mount("#app");
